8e3e4e73f2e1ffaea3ef462d9b1be7a1334dbdb2
mbpmm <marianomcr92@gmail.com>
2019-06-23 23:15:25 -0300

    Explosion WIP

---
+++ MoonLanderDVJ2/Assets/Game/Scripts/ExplosionController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionController : MonoBehaviour
+{
+    public float lifeSpan;
+    public GameObject cam;
+    private CameraShake camShake;
+    private float timer;
+
+    private void Start()
+    {
+        cam = GameObject.Find("Main Camera");
+        camShake = cam.GetComponent<CameraShake>();
+        StartCoroutine(camShake.Shake(.3f, .2f));
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+        
+        if (timer>lifeSpan)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- MoonLanderDVJ2/Assets/Game/Scripts/PlayerController.cs
+++ MoonLanderDVJ2/Assets/Game/Scripts/PlayerController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public GameObject camera;
+    public GameObject explosion;
     public float speed;
     public float rotSpeed;
     public float fuel;
     public ParticleSystem fire;
     public LayerMask raycastLayer;
+    public bool onGround;
     int rayDistance = 2;
     private float rotLimit = 90f;
     private Rigidbody2D playerRB;
@@ -51,9 +53,20 @@ public class PlayerController : MonoBehaviour
         if (euler.z > 180) euler.z = euler.z - 360;
         euler.z = Mathf.Clamp(euler.z, -rotLimit, rotLimit);
         transform.eulerAngles = euler;
+       // Debug.Log(playerRB.velocity.y);
+        //if (onGround && playerRB.velocity.y>0.01f)
+        //{
+        //    Explode();
+        //}
 
     }
 
+    public void Explode()
+    {
+        GameObject expAux;
+        expAux = Instantiate(explosion, transform.position, Quaternion.identity);
+        gameObject.SetActive(false);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         cam.fieldOfView = 40;
@@ -67,4 +80,26 @@ public class PlayerController : MonoBehaviour
         camFollow.boundX = 4.15f;
         camFollow.boundY = 2.3f;
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag=="Level")
+        {
+            if ( playerRB.velocity.y < -0.4f)
+            {
+                Debug.Log("por que no funciona");
+                Explode();
+            }
+            else
+                onGround = true;
+        }
+    }
+
+    //private void OnCollisionExit2D(Collision2D collision)
+    //{
+    //    if (collision.gameObject.tag == "Level")
+    //    {
+    //        onGround = false;
+    //    }
+    //}
 }
